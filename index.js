4d657e06bf571d93d68245c88de6a208205748e4
BondT1 <tombond248@gmail.com>
2023-06-08 18:59:16 +0100

    started questions function

--- index.js
+++ index.js
@@ -5,7 +5,50 @@ const Markdown = require('./utils/generateMarkdown');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+        validate: title => {
+            if (title) {
+                return true;
+            } else {
+                console.log('You must enter a title.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'whatProject',
+        message: 'What is your project?',
+        validate: projectWhat => {
+            if (projectWhat) {
+                return true;
+            } else {
+                console.log('Please enter a description of what your project is.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'projectPurpose',
+        message: 'What is the purpose of your project?',
+        validate: purpose => {
+            if (purpose) {
+                return true;
+            } else {
+                console.log('Please enter the purpose of your project.');
+                return false;
+            }
+        }
+    },
+
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
